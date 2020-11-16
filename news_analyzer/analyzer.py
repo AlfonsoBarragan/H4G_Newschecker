b143843be47092a9b1e06f789b94ab8165543bac
Diego Sepúlveda Millán <41056207+DiegoSepuMillan@users.noreply.github.com>
2020-11-16 20:43:35 +0100

    Lexical analysis working!

--- news_analyzer/analyzer.py
+++ news_analyzer/analyzer.py
@@ -1,13 +1,15 @@
 import pandas
 import nltk
 import time
-import utils
 import random
 import re
 import routes
+import numpy as np
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from translate import Translator
 
 from operator import itemgetter
 
@@ -60,6 +62,8 @@ def converse_to_tokens(words_to_tokenize):
     similarity_list     = []
     smaller_similarity   = 0
     
+    translator= Translator(to_lang="en", from_lang="es")    
+
     # Lists pronouns
 
     pronombres_det_demostrativos            = ['aquel', 'aquella', 'aquellos','aquellas','esa','esas',
@@ -75,44 +79,64 @@ def converse_to_tokens(words_to_tokenize):
                                                 'ninguna', 'ningunas', 'ninguno', 'ningún', 'ningunos', 'otra', 'otras', 'otro', 'otros',
                                                 'sendas', 'sendos', 'tantas', 'tantos', 'tanto', 'todas', 'toda', 'todos', 'todo', 'unas',
                                                 'una', 'unos', 'un', 'varias', 'varios']
+    
+    determinantes_articulos                 = ['el', 'la', 'los', 'las']
 
     pronombres_personales_tonicos           = ['yo', 'tu', 'él', 'ella', 'nosotros', 'nosotras', 'vosotros', 'vosotras', 'ellos', 'ellas']
 
     pronombres_personales_atonos            = ['me', 'te', 'se', 'nos' 'os']
     
-    relative_pronouns_list                  = ['that', 'which', 'who', 'whom',
-                                                'whose', 'whichever', 'whoever', 'whomever']
+    preposiciones                           = ['a', 'al', 'ante', 'bajo', 'cabe', 'con', 'contra', 'de', 'del', 'desde', 'en', 'entre', 'hacia', 'hasta', 'para', 
+                                               'por', 'según', 'sin', 'sobre', 'tras', 'durante', 'mediante']
+    
+    number_expresion                        = re.compile(r"[\d]+[%+\-*]*")
+    nombre_propio_expresion                 = re.compile(r"[A-Z][a-z]+")
+    siglas_expresion                        = re.compile(r"[A-Z][A-Z]+")
+    
     
     # Processing_words
     
     for word in words_to_tokenize:
         word_index              = words_to_tokenize.index(word)
+        word = word.replace('“', '')
         similarity_list         = []
         smaller_similarity       = 0
         has_meaning             = False
         
-        if word in pronombres_personales_tonicos:
+        if word.lower() in pronombres_personales_tonicos:
             token_list.append(word_token(word, None, 'pronombre_personal_tonico_token', 0))
             continue
 
-        elif  word in pronombres_det_posesivos:
+        elif  word.lower() in pronombres_det_posesivos:
             token_list.append(word_token(word, None, 'pronombres_det_posesivos_token', 0))
             continue
 
-        elif word in determinantes_interrogativos:
+        elif word.lower() in determinantes_interrogativos:
             token_list.append(word_token(word, None, 'determinantes_interrogativos_token', 0))
             continue
 
-        elif word in determinantes_indefinidos:
+        elif word.lower() in determinantes_indefinidos:
             token_list.append(word_token(word, None, 'determinantes_indefinidos_token', 0))
             continue
 
-        elif word in pronombres_personales_tonicos:
+        elif word.lower() in determinantes_articulos:
+            token_list.append(word_token(word, None, 'determinantes_articulos_token', 0))
+            continue
+
+        elif word.lower() in pronombres_personales_tonicos:
             token_list.append(word_token(word, None, 'pronombres_personales_tonicos_token', 0))
             continue
 
-        elif word in pronombres_personales_atonos:
+        elif word.lower() in pronombres_personales_atonos:
             token_list.append(word_token(word, None, 'pronombres_personales_atonos_token', 0))
+            continue
+
+        elif word.lower() in preposiciones:
+            token_list.append(word_token(word, None, 'preposiciones_token', 0))
+            continue        
+        
+        elif number_expresion.findall(word) != []:
+            token_list.append(word_token(word, None, 'numeric_expresion_token', 0))
             continue
 
         else:
@@ -123,65 +147,90 @@ def converse_to_tokens(words_to_tokenize):
                 synsets_list        = wn.synsets(word, lang='spa')
                 next_synsets_list   = wn.synsets(words_to_tokenize[word_index - 1], lang='spa')
 
-            if synsets_list > next_synsets_list:
-                bigger_list     = synsets_list
-                smaller_list    = next_synsets_list
-            else:
-                bigger_list     = next_synsets_list
-                smaller_list    = synsets_list
-
-            for big_synset in  bigger_list:
-                similarities = map(big_synset.path_similarity, smaller_list)
-                similarity_list.append(list(similarities))
-
-            for index in range(len(similarity_list)):
-
-                aux_list = [similarity_list[index].index(simil) for simil in similarity_list[index] if simil != None]
-
-                for index_simil in aux_list:
-                    if similarity_list[index][index_simil] < smaller_similarity:
-                        smaller_similarity = similarity_list[index][index_simil]
-
-            # Sometimes all the meanings had the same shortest path to the next word
-            # so, we take only the first of all the coincidence
-            for simil in similarity_list:
-                if bigger_list == synsets_list and has_meaning == False:
-                    if smaller_similarity in simil:
-                        meaning_def = wn.synsets(word, lang='spa')[similarity_list.index(simil)]
-                        kind_of_word = meaning_def.name().split('.')
-
-                        if word != 'okay':
-                            token_list.append(word_token(word, meaning_def.definition(), kind_of_word[1], 1))
-                        else:
-                            token_list.append(word_token(word, meaning_def.definition(), kind_of_word[3], 1))
-                        has_meaning = True
-
-                elif bigger_list == next_synsets_list and has_meaning == False:
-                    if smaller_similarity in simil:
-                        meaning_def = wn.synsets(word, lang='spa')[simil.index(smaller_similarity)]
-                        kind_of_word = meaning_def.name().split('.')
-
-                        if word != 'okay':
-                            token_list.append(word_token(word, meaning_def.definition(), kind_of_word[1], 1))
-                        else:
-                            token_list.append(word_token(word, meaning_def.definition(), kind_of_word[3], 1))
-                        has_meaning = True
-                
-            if has_meaning == False and len(wn.synsets(word, lang='spa')) > 0:
-                meaning_def = wn.synsets(word, lang='spa')[0]
-                kind_of_word = meaning_def.name().split('.')
-
-                if word != 'okay':
-                    token_list.append(word_token(word, meaning_def.definition(), kind_of_word[1], 1))
+            if next_synsets_list != []:
+                if synsets_list > next_synsets_list:
+                    bigger_list     = synsets_list
+                    smaller_list    = next_synsets_list
                 else:
-                    token_list.append(word_token(word, meaning_def.definition(), kind_of_word[3], 1))
-
-                has_meaning = True
-        
+                    bigger_list     = next_synsets_list
+                    smaller_list    = synsets_list
+    
+                for big_synset in  bigger_list:
+                    similarities = map(big_synset.path_similarity, smaller_list)
+                    similarity_list.append(list(similarities))
+    
+                for index in range(len(similarity_list)):
+    
+                    aux_list = [similarity_list[index].index(simil) for simil in similarity_list[index] if simil != None]
+    
+                    for index_simil in aux_list:
+                        if similarity_list[index][index_simil] < smaller_similarity:
+                            smaller_similarity = similarity_list[index][index_simil]
+    
+                # Sometimes all the meanings had the same shortest path to the next word
+                # so, we take only the first of all the coincidence
+                for simil in similarity_list:
+                    if bigger_list == synsets_list and has_meaning == False:
+                        if smaller_similarity in simil:
+                            meaning_def = [similarity_list.index(simil)]
+                            kind_of_word = meaning_def.name().split('.')
+    
+                            if word != 'okay':
+                                token_list.append(word_token(word, meaning_def.definition(), kind_of_word[1], 1))
+                            else:
+                                token_list.append(word_token(word, meaning_def.definition(), kind_of_word[3], 1))
+                            has_meaning = True
+    
+                    elif bigger_list == next_synsets_list and has_meaning == False:
+                        if smaller_similarity in simil:
+                            meaning_def = wn.synsets(word, lang='spa')[simil.index(smaller_similarity)]
+                            kind_of_word = meaning_def.name().split('.')
+    
+                            if word != 'okay':
+                                token_list.append(word_token(word, meaning_def.definition(), kind_of_word[1], 1))
+                            else:
+                                token_list.append(word_token(word, meaning_def.definition(), kind_of_word[3], 1))
+                            has_meaning = True
+                    
+                if has_meaning == False and len(wn.synsets(word, lang='spa')) > 0:
+                    meaning_def = wn.synsets(word, lang='spa')[0]
+                    kind_of_word = meaning_def.name().split('.')
+    
+                    if word != 'okay':
+                        token_list.append(word_token(word, meaning_def.definition(), kind_of_word[1], 1))
+                    else:
+                        token_list.append(word_token(word, meaning_def.definition(), kind_of_word[3], 1))
+    
+                    has_meaning = True
+            else:
+                try:
+                    if word != "":                
+                        if nombre_propio_expresion.findall(word) != [] or siglas_expresion.findall(word):    
+                            
+                            if wn.synsets(word, lang='spa') != []:
+                                token_list.append(word_token(word, wn.synsets(word, lang='spa')[0], wn.synsets(word, lang='spa')[0].name().split('.')[1], 1))
+                            
+                            else:
+                                token_list.append(word_token(word, None, 'nombres_propios_token', 0))
+    
+                        
+                        else:
+                            word_traduced = translator.translate(word).lower().split(' ')
+                        
+                            if len(word_traduced) > 1:
+                                word_traduced = word_traduced[len(word_traduced) - 1]
+                            else:
+                                word_traduced = word_traduced[0]
+                            
+                            token_list.append(word_token(word, wn.synsets(word_traduced)[0], wn.synsets(word_traduced)[0].name().split('.')[1], 1))
+                except:
+                    continue
+                    
     return token_list
-                
+
 def lexical_analysis(descriptions_complete):
     # Taking full descriptions and split by lines
+    
     descriptions_list           = descriptions_complete.split('\n')
     descriptions_splitted       = []
     descriptions_corrected      = []
@@ -284,21 +333,20 @@ def split_descriptions(description):
 
     return line_fragments_1
 
-def write_lexical_analysis_result(descriptions_tokens_sequence):
+def write_lexical_analysis_result(descriptions):
     
     file_output = open(routes.lexical_sequence, 'w')
 
-    for description in descriptions_tokens_sequence:
-        for phrases in description:
-            phrase_to_write = ""
-            
-            for word in phrases:
-                print(word.word)
-                print("{}: {}".format(word.word, word.lex_class))
-                phrase_to_write += "{} ".format(word.lex_class)
-
-            phrase_to_write += "\n"
-            file_output.write(phrase_to_write)
+    for descriptions_tokens_sequence in descriptions:    
+        for description in descriptions_tokens_sequence:
+            for phrases in description:
+                phrase_to_write = ""
+                
+                for word in phrases:
+                    phrase_to_write += "{} ".format(word.lex_class)
+    
+                phrase_to_write += "\n"
+                file_output.write(phrase_to_write)
 
     print("[*] Tokens dataset write!")
 
@@ -373,24 +421,28 @@ def compose_dataset_to_sintactic_analysis():
     col_names = ['previous_token', 'actual_token', 'next_token',
                  'first_word', 'last_word']
     
-    return dictionary_from_list
-    pass
+    return pd.DataFrame(np.array(list_to_dataframe), columns=col_names)
 
 # Functions to analyze descriptions (SEMANTIC)
 
 def analyze_exec():
 
     print("[*] Beggining preprocess and analysis...")
-    news_text = load_news_text(route_database)
+    news_text = load_news_text(routes.database_example)
 
-    return lexical_analysis(traduced_descriptions)
+    return lexical_analysis(news_text)
 
 def mix_tokens_with_database(route_database, route_tokens_sequence):
     dataframe_database          = load_news_text(route_database)
-    dataframe_tokens_sequence   = lexical_analysis(read_traduced_descriptions_file()) 
+    
+    descriptions_list           = list(dataframe_database['text'])
+    lexical_sequences           = []
+    
+    for description in descriptions_list:
+         
+        lexical_sequences.append(lexical_analysis(description))        
 
-    print(dataframe_database)
-    print(len(dataframe_tokens_sequence))
+    return lexical_sequences
 
 def exec_routine(list_execution):
     execution_orders = []
